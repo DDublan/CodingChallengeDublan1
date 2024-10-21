453e138f14d7266f38b5aaf40de088f26e764d52
Dereck Dublan <750284@student.dusd.net>
2024-10-21 12:00:02 -0700

    nerd

--- CodingChallengeDublan1/Program.cs
+++ CodingChallengeDublan1/Program.cs
@@ -1,115 +1,145 @@
 ﻿using System;
-using System.Runtime.ConstrainedExecution;
+
 
 class Challenges
 {
     public static void Main(string[] args)
     {
+        bool run = true;
+        while (run == true)
+        {
+            Console.WriteLine("Welcome to my coding challenge program. There will be a variety of different functions you will have to choose from. Please pick one of these numbers:");
+            Console.WriteLine("1: Adding Numbers");
+            Console.WriteLine("2: Converting Minutes to seconds");
+            Console.WriteLine("3: Adding 1 to any given number");
+            Console.WriteLine("4: Calculating Circut Power");
+            Console.WriteLine("5: Converting age (in years) to days old");
+            Console.WriteLine("6: Finding the area of a triangle");
+            Console.WriteLine("7: Finding out if a number is less than 0");
+            Console.WriteLine("0: exit program");
 
-        Console.WriteLine("Welcome to my coding challenge program. There will be a variety of different functions you will have to choose from. Please pick one.");
+            var choice = Console.ReadLine();
+            int choiceint = int.Parse(choice);
+            if (choiceint == 0)
+            {
+                run = false;
+            }
+            else if (choiceint == 1)
+            {
+                Console.WriteLine("Today we are going to use the number added called SUM. Please input two numbers for us to add. \n");
 
-        Console.WriteLine("Today we are going to usse the number adder called SUM. Please input two numbers for us to add. \n");
+                var number1 = Console.ReadLine();
+                int number1int = int.Parse(number1);
+                Console.WriteLine("Awesome! Please input a second number.");
 
-        var number1 = Console.ReadLine();
-        int number1int = int.Parse(number1);
+                var number2 = Console.ReadLine();
+                int number2int = int.Parse(number2);
 
-        Console.Write("Wow! Your stupid! Please Input one more number!");
+                Console.WriteLine("The sum of the number: " + number1 + " and the number: " + number2 + " is equal to: " + Sum(number1int, number2int));
+            }
+            else if (choiceint == 2)
+            {
+                Console.WriteLine("Let's try converting minutes into seconds. Give me a number to convert minutes to seconds.");
+                var number3 = Console.ReadLine();
+                int number3int = int.Parse(number3);
 
-        var number2 = Console.ReadLine();
-        int number2int = int.Parse(number2);
+                Console.WriteLine(number3 + " Minutes converted to seconds is: " + Convert(number3int) + " seconds ");
 
-        Console.WriteLine("The sum of the number: " + number1 + " and the number: number2 " + "is equal to: " + Sum(number1int, number2int));
+            }
+            else if (choiceint == 3)
+            {
+                Console.WriteLine("Now, give me a number that I will add a digit to.");
+                var appendicitis = Console.ReadLine();
+                int appendicitisint = int.Parse(appendicitis);
 
-        Console.WriteLine("Let's try converting minutes into seconds. Give me a number to convert minutes to seconds.");
-        var number3 = Console.ReadLine();
-        int number3int = int.Parse(number3);
+                Console.WriteLine(appendicitis + " Your number plus 1 is equal to: " + AddOne(appendicitisint));
+            }
+            else if (choiceint == 4)
+            {
+                Console.WriteLine("Let's calculatethe circut power. \n");
 
-        Console.WriteLine(number3 + " Minutes converted to seconds is: " + Convert(number3int) + " seconds " );
+                Console.WriteLine("Give me a value for your voltage.");
+                var plague = Console.ReadLine();
+                int plagueint = int.Parse(plague);
 
-        Sum(number1int, number2int);
+                Console.WriteLine("Now give me a value for your current.");
+                var stupidity = Console.ReadLine();
+                int stupidityint = int.Parse(stupidity);
 
-        Console.WriteLine("Now input any number and I will add 1 digit to it.");
-        var appendicitis = Console.ReadLine();
-        int appendicitisint = int.Parse(appendicitis);
+                Console.WriteLine("Your circut power is equal to: " + VoltConvert(shocktuahint, lightningonthatthangint));
+                VoltConvert(plagueint, stupidityint);
+            }
+            else if (choiceint == 5)
+            {
+                Console.WriteLine("Now tell me how many years old you are so I can convert it to days");
+                var urmother = Console.ReadLine();
+                int urmotherint = int.Parse(urmother);
 
-        Console.WriteLine(appendicitis + " and add 1 digit and you get: " + AddOne(appendicitisint));
+                Console.WriteLine("You are: " + Oldie(urmotherint) + " days old.");
+            }
+            else if (choiceint == 6)
+            {
+                Console.WriteLine("Now let's find the area of a triangle, first give me a height.");
+                var height = Console.ReadLine();
+                int heightint = int.Parse(height);
 
-        Console.WriteLine("Let's try calculating circuit power! \n");
+                Console.WriteLine("Now give me the base of your triangle.");
+                var width = Console.ReadLine();
+                int widthint = int.Parse(width);
 
-        Console.WriteLine("Give me a value for your voltage.");
-        var stupidity = Console.ReadLine();
-        int stupidityint = int.Parse(stupidity);
+                Console.WriteLine("The area of your triangle is: " + area(heightint, widthint));
+                area(heightint, widthint);
+            }
+            else if (choiceint == 7)
+            {
+                Console.WriteLine("Now give me a number and I will see if it's less then 0. \n please give me a number to look at and check");
+                var number10 = Console.ReadLine();
+                int number10int = int.Parse(number10);
 
-        Console.WriteLine("Give me the current value human.");
-        var foolishness = Console.ReadLine();
-        int foolishnessint = int.Parse(foolishness);
-
-        Console.WriteLine("Your circuit power is equal to: " + VoltConvert(stupidityint, foolishnessint));
-        VoltConvert(stupidityint, foolishnessint);
-
-        Console.WriteLine("Now I will convert years to days. Give me a number Juniper.");
-        var whopperjuniordoubletripplewhopper = Console.ReadLine();
-        int whopperjuniordoubletripplewhopperint = int.Parse(whopperjuniordoubletripplewhopper);
-
-        Console.WriteLine("You are: " + CalcAge(whopperjuniordoubletripplewhopperint) + " days old.");
-
-        Console.WriteLine("Now let's find the area of a triangle, first me give a height.");
-        var walterwhite = Console.ReadLine();
-        int walterwhiteint = int.Parse(walterwhite);
-
-        Console.WriteLine("Now igve me the base of your triangle");
-        var jessepinkman = Console.ReadLine();
-        int jessepinkmanint = int.Parse(jessepinkman);
-
-        Console.WriteLine("THE AREA 51 OF YOUR TRIANGLE IS: " + CalcArea(walterwhiteint, jessepinkmanint)); 
-        CalcArea(walterwhiteint, jessepinkmanint);
-
-        Console.WriteLine("Now give me a number and I will see if it's greater or less than 0. \n please give me a number");
-        var snickerdoodle = Console.ReadLine();
-        int snickerdoodleint = int.Parse(snickerdoodle);
-
-        Console.WriteLine(lospolloshermanos(snickerdoodleint));
-
+                Console.WriteLine(lessthanorequaltozero(number10int));
+            }
+            else
+            {
+                Console.WriteLine("Invaled number, please type an avaible number");
+            }
+        }
     }
 
     public static int Sum(int number1, int number2)
-    { return number1 + number2; }
+    {
+        return number1 + number2;
+    }
 
     public static int Convert(int number)
-
     {
         return number * 60;
     }
-
     public static int AddOne(int appendicitis)
     {
         return appendicitis += 1;
     }
 
-    public static int VoltConvert(int stupidity, int foolishness)
+    public static int VoltConvert(int plague, int stupidity)
     {
-        return stupidity * foolishness;
+        return plague * stupidity;
     }
 
-    public static int CalcAge(int whopperjuniordoubletripplewhopper)
+    public static int Oldie(int urmother)
     {
-        return whopperjuniordoubletripplewhopper * 365;
+        return urmother * 365;
     }
 
-    public static float CalcArea(float walterwhite, int jessepinkman)
+    public static float area(float height, int width)
     {
-        return (walterwhite * jessepinkman) / 2;
+        return (height * width) / 2;
     }
 
-    public static bool lospolloshermanos(int snickerdoodle)
+    public static bool lessthanorequaltozero(int number10)
     {
-        if (snickerdoodle <= 0)
+        if (number10 <= 0)
         {
             return true;
         }
         return false;
     }
 }
-
-
-
